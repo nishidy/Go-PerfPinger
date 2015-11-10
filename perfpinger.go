77852258f5f6176d31be0ccdbbec0694f5ec9836
nishidy <nishidyatutokyo@gmail.com>
2015-11-10 22:08:51 +0900

    Run ping in loop and stop it by sigint.

--- perfpinger.go
+++ perfpinger.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 	"time"
 
 	"golang.org/x/net/icmp"
@@ -27,6 +29,12 @@ func main() {
 		os.Exit(2)
 	}
 
+	interval, err := strconv.ParseInt(os.Args[3], 10, 64)
+	if err != nil {
+		fmt.Println(os.Stderr, "Failed to convert with ", os.Args[3])
+		os.Exit(7)
+	}
+
 	conn, err := icmp.ListenPacket("udp4", "")
 	if err != nil {
 		fmt.Println(os.Stderr, "Failed to listen upd4")
@@ -39,32 +47,53 @@ func main() {
 		data[i] = 1
 	}
 
-	bytes, err := (&icmp.Message{
-		Type: ipv4.ICMPTypeEcho,
-		Code: 0,
-		Body: &icmp.Echo{
-			ID: 0, Seq: 0,
-			Data: data,
-		},
-	}).Marshal(nil)
-	if err != nil {
-		fmt.Println(os.Stderr, "Failed to create ICMP message")
-		os.Exit(4)
-	}
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT)
 
-	size, err := conn.WriteTo(bytes, &net.UDPAddr{IP: ra.IP, Zone: ra.Zone})
-	if err != nil {
-		fmt.Println(os.Stderr, "Failed to write ICMP message", err)
-		os.Exit(5)
-	}
+	exitchan := make(chan int)
+	go func() {
+		t := time.NewTicker(time.Duration(interval) * time.Millisecond)
+		for {
+			select {
+			case <-t.C:
+				bytes, err := (&icmp.Message{
+					Type: ipv4.ICMPTypeEcho,
+					Code: 0,
+					Body: &icmp.Echo{
+						ID: 0, Seq: 0,
+						Data: data,
+					},
+				}).Marshal(nil)
+				if err != nil {
+					fmt.Println(os.Stderr, "Failed to create ICMP message")
+					os.Exit(4)
+				}
 
-	conn.SetReadDeadline(time.Now().Add(time.Millisecond * 500))
-	size, addr, err := conn.ReadFrom(bytes)
-	if err != nil {
-		fmt.Println(os.Stderr, "Failed to read ICMP message", err)
-		os.Exit(6)
-	}
+				size, err := conn.WriteTo(bytes, &net.UDPAddr{IP: ra.IP, Zone: ra.Zone})
+				if err != nil {
+					fmt.Println(os.Stderr, "Failed to write ICMP message", err)
+					os.Exit(5)
+				}
 
-	fmt.Println(size, addr, bytes)
+				conn.SetReadDeadline(time.Now().Add(time.Millisecond * 500))
+				size, addr, err := conn.ReadFrom(bytes)
+				if err != nil {
+					fmt.Println(os.Stderr, "Failed to read ICMP message", err)
+					os.Exit(6)
+				}
+
+				fmt.Println(size, addr, bytes)
+
+			case <-sigchan:
+				exitchan <- 1
+
+			}
+		}
+		t.Stop()
+	}()
+
+	_ = <-exitchan
+
+	fmt.Println("Bye.")
 
 }

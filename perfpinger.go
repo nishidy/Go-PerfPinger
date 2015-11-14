675cd09ac8b684bff4956177865ddfd6a6483344
nishidy <nishidyatutokyo@gmail.com>
2015-11-14 10:36:01 +0900

    Enabled mulitple ping processes.

--- perfpinger.go
+++ perfpinger.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"math/rand"
 	"net"
@@ -12,122 +14,215 @@ import (
 	"time"
 
 	"golang.org/x/net/icmp"
+	"golang.org/x/net/internal/iana"
 	"golang.org/x/net/ipv4"
 )
 
-func main() {
+type PingNet struct {
+	addr *net.IPAddr
+	conn *icmp.PacketConn
+}
 
-	hostname := os.Args[1]
-	ra, err := net.ResolveIPAddr("ip4", hostname)
+type PingData struct {
+	id   int
+	data []byte
+	size int
+	intv uint64
+}
+
+type PingNum struct {
+	sends    int
+	receives int
+}
+
+type PingChan struct {
+	exitch chan int
+	collch chan int
+}
+
+type Ping struct {
+	PingNet
+	PingData
+	PingNum
+	PingChan
+}
+
+type Reply struct {
+	addr  net.Addr
+	size  int
+	bytes []byte
+}
+
+func (p *Ping) doPing() {
+
+	fmt.Printf("PERFPINGER %s (ID:%d): %d data bytes\n", p.addr.IP, p.id, p.size)
+
+	t := time.NewTicker(
+		time.Duration(p.intv) * time.Millisecond,
+	)
+
+	for {
+		select {
+		case <-t.C:
+			wbytes, err := (&icmp.Message{
+				Type: ipv4.ICMPTypeEcho,
+				Code: 0,
+				Body: &icmp.Echo{
+					ID: p.id, Seq: p.sends,
+					Data: p.data,
+				},
+			}).Marshal(nil)
+			if err != nil {
+				fmt.Println(os.Stderr, "Failed to create ICMP message")
+				os.Exit(4)
+			}
+
+			_, err = p.conn.WriteTo(wbytes, &net.IPAddr{IP: p.addr.IP})
+			if err != nil {
+				fmt.Println(os.Stderr, "Failed to write ICMP message", err)
+				os.Exit(5)
+			}
+
+			p.conn.SetReadDeadline(
+				time.Now().Add(time.Duration(p.intv) * time.Millisecond),
+			)
+
+			rbytes := make([]byte, p.size+8)
+			size, addr, err := p.conn.ReadFrom(rbytes)
+
+			if err != nil {
+				fmt.Printf("* Timed out from %s: icmp_seq=%d\n", p.addr.IP, p.sends)
+			} else {
+				if p.parseMessage(Reply{addr, size, rbytes}) {
+					p.receives += 1
+				}
+			}
+			p.sends += 1
+
+		case <-p.exitch:
+			p.collch <- p.receives
+			return
+
+		}
+	}
+}
+
+func (p *Ping) parseMessage(r Reply) bool {
+	rep, err := icmp.ParseMessage(iana.ProtocolICMP, r.bytes)
 	if err != nil {
-		fmt.Println(os.Stderr, "Failed to resolve with ", hostname, err)
-		os.Exit(1)
+		fmt.Printf("* Reply error from %s: icmp_seq=%d -> %s\n", r.addr, p.sends, r.bytes)
+		return false
+	}
+	if strings.Split(p.addr.String(), ":")[0] != p.addr.IP.String() {
+		fmt.Printf("* Replied from %s: icmp_seq=%d\n", r.size, r.addr, p.sends)
+		return false
+	}
+	return p.parseMessageBody(rep, r)
+}
+
+func (p *Ping) parseMessageBody(rep *icmp.Message, r Reply) bool {
+	switch pkt := rep.Body.(type) {
+	case *icmp.Echo:
+		if !(pkt.ID == p.id && pkt.Seq == p.sends && bytes.Equal(pkt.Data, p.data)) {
+			fmt.Printf("* Wrong data %d bytes from %s (ID=[%d,%d], Seq=[%d,%d]): icmp_seq=%d\n",
+				r.size, r.addr, pkt.ID, p.id, pkt.Seq, p.sends, p.sends)
+			return false
+		}
+	default:
+		fmt.Printf("* Not echo received from %s: icmp_seq=%d\n", r.size, r.addr, p.sends)
+		return false
 	}
 
-	datalen, err := strconv.Atoi(os.Args[2])
+	fmt.Printf("%d bytes from %s: icmp_seq=%d\n", r.size, r.addr, p.sends)
+	return true
+}
+
+func main() {
+
+	if len(os.Args) < 4 {
+		os.Exit(10)
+	}
+
+	hostfile := os.Args[1]
+	hdlr, err := os.Open(hostfile)
+	if err != nil {
+		fmt.Println(os.Stderr, "Failed to open with ", hostfile, err)
+		os.Exit(11)
+	}
+	defer hdlr.Close()
+
+	size, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		fmt.Println(os.Stderr, "Failed to convert with ", os.Args[2])
 		os.Exit(2)
 	}
 
-	interval, err := strconv.ParseInt(os.Args[3], 10, 64)
+	intv, err := strconv.ParseUint(os.Args[3], 10, 64)
 	if err != nil {
 		fmt.Println(os.Stderr, "Failed to convert with ", os.Args[3])
 		os.Exit(7)
 	}
 
-	conn, err := icmp.ListenPacket("udp4", "")
-	if err != nil {
-		fmt.Println(os.Stderr, "Failed to listen upd4")
-		os.Exit(3)
-	}
-	defer conn.Close()
-
-	data := make([]byte, datalen)
-	for i := 0; i < datalen; i++ {
+	data := make([]byte, size)
+	for i := 0; i < size; i++ {
 		data[i] = uint8(i)
 	}
 
 	rand.Seed(time.Now().UnixNano())
-	id := rand.Intn(65535)
 
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT)
+	exitch := make(chan int, 1)
+	collch := make(chan int, 1)
 
-	sends := 0
-	receives := 0
+	pingnum := 0
+	scanner := bufio.NewScanner(hdlr)
+	for scanner.Scan() {
+		host := scanner.Text()
 
-	exitchan := make(chan int)
+		addr, err := net.ResolveIPAddr("ip4", host)
+		if err != nil {
+			fmt.Println(os.Stderr, "Failed to resolve with ", host, err)
+			os.Exit(1)
+		}
+
+		conn, err := icmp.ListenPacket("ip4:icmp", "10.1.1.108")
+		if err != nil {
+			fmt.Println(os.Stderr, "Failed to listen ip4:icmp (%s)", err)
+			os.Exit(3)
+		}
+		defer conn.Close()
+
+		id := rand.Intn(65535)
+
+		p := &Ping{
+			PingNet{addr, conn},
+			PingData{id, data, size, intv},
+			PingNum{0, 0},
+			PingChan{exitch, collch},
+		}
+
+		go p.doPing()
+
+		pingnum += 1
+	}
+
+	sigch := make(chan os.Signal, 1)
+	signal.Notify(sigch, syscall.SIGINT)
+
+	allrecs := 0
 	go func() {
-		t := time.NewTicker(time.Duration(interval) * time.Millisecond)
-		for {
-			select {
-			case <-t.C:
-				bytes, err := (&icmp.Message{
-					Type: ipv4.ICMPTypeEcho,
-					Code: 0,
-					Body: &icmp.Echo{
-						ID: id, Seq: sends,
-						Data: data,
-					},
-				}).Marshal(nil)
-				if err != nil {
-					fmt.Println(os.Stderr, "Failed to create ICMP message")
-					os.Exit(4)
-				}
-
-				_, err = conn.WriteTo(bytes, &net.UDPAddr{IP: ra.IP, Zone: ra.Zone})
-				if err != nil {
-					fmt.Println(os.Stderr, "Failed to write ICMP message", err)
-					os.Exit(5)
-				}
-
-				conn.SetReadDeadline(time.Now().Add(time.Duration(interval) * time.Millisecond))
-				size, addr, err := conn.ReadFrom(bytes)
-				if err != nil {
-					fmt.Printf("Timed out from %s: icmp_seq=%d\n", ra.IP, sends)
-				} else {
-
-					var rep *icmp.Message
-					rep, err := icmp.ParseMessage(1, bytes)
-					if err != nil {
-						fmt.Printf("Reply error from %s: icmp_seq=%d\n", addr, sends)
-					} else {
-
-						if strings.Split(addr.String(), ":")[0] != ra.IP.String() {
-							fmt.Printf("%d bytes from %s !?: icmp_seq=%d\n", size, addr, sends)
-						} else {
-							switch pkt := rep.Body.(type) {
-							case *icmp.Echo:
-								if pkt.ID == id {
-									fmt.Printf("%d bytes from %s: icmp_seq=%d\n",
-										size, addr, sends)
-									receives += 1
-								}
-							default:
-								return
-							}
-						}
-
-					}
-
-				}
-				sends += 1
-
-			case <-sigchan:
-				exitchan <- 1
-				return
-
-			}
+		_ = <-sigch
+		close(exitch)
+		for i := 0; i < pingnum; i++ {
+			allrecs += <-collch
 		}
 	}()
 
 	start := time.Now()
-	_ = <-exitchan
+	_ = <-exitch
 	finish := time.Now()
 
 	dur := finish.Sub(start).Seconds() / time.Second.Seconds()
-	thr := float64(receives) * (float64(datalen) * 8) / (dur * 1024 * 2)
-	fmt.Printf("%d packets in %.2f sec : %.2f Kbps of both UL and DL.\n", receives, dur, thr)
+	thr := float64(allrecs) * (float64(size) * float64(pingnum) * 8) / (dur * 1024 * 2)
+	fmt.Printf("%d packets in %.2f sec : %.2f Kbps of both UL and DL.\n", allrecs, dur, thr)
 
 }
